632b827b2cd33ccc3dd9aaf318e1c95018fc6e1c
Johannes Diemke <johannesdiemke128@gmail.com>
2016-05-16 00:03:38 +0200

    Added tests.

---
+++ library/src/test/java/io/github/jdiemke/triangulation/TriangleSoupTest.java
@@ -0,0 +1,20 @@
+package io.github.jdiemke.triangulation;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TriangleSoupTest {
+
+    @Test
+    public void testThatTriangleSoupIsInitialyEmpty() {
+        TriangleSoup soup = new TriangleSoup();
+        List<Triangle2D> triangles = soup.getTriangles();
+
+        Assert.assertNotNull("The returned list should not be null.", triangles);
+        Assert.assertEquals("The triangle soup should not contain any triangles if no triangles were added.", 0,
+                triangles.size());
+    }
+
+}
