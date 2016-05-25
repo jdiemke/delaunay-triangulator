34874ab7dc9b29145eaa249b4da7ef35b959c500
Johannes Diemke <johannesdiemke128@gmail.com>
2016-05-26 01:57:54 +0200

    Added another test case

--- library/src/test/java/io/github/jdiemke/triangulation/TriangleSoupTest.java
+++ library/src/test/java/io/github/jdiemke/triangulation/TriangleSoupTest.java
@@ -17,4 +17,15 @@ public class TriangleSoupTest {
                 triangles.size());
     }
 
+    @Test
+    public void testAddTriangle() {
+        TriangleSoup soup = new TriangleSoup();
+        soup.add(new Triangle2D(new Vector2D(0, 0), new Vector2D(1, 0), new Vector2D(1, 1)));
+
+        List<Triangle2D> triangles = soup.getTriangles();
+
+        Assert.assertNotNull("The returned list should not be null.", triangles);
+        Assert.assertEquals("The triangle soup should contain exactly one triangle.", 1, triangles.size());
+    }
+
 }
